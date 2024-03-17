27c417c82c262f63da41506a5b2b9268c86f9a56
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2024-03-17 17:48:24 +0000

    Add brew to path after installing it

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,4 +7,6 @@ then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
 brew bundle --file=$HOME/.dotfiles/Brewfile
