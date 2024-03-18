2b5008e6a5833c2401982c87567851589ee967b6
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2024-03-18 22:05:12 +0000

    Fix issue with compinit insecure directories

--- zsh/fpath.sh
+++ zsh/fpath.sh
@@ -1,5 +1,5 @@
 # Add each topic folder to fpath so that they can add functions and completion scripts
-for topic_folder in $ZSH/*; do
+for topic_folder in $DOTFILES/*; do
 	if [ -d "$topic_folder" ]; then
 		fpath=("$topic_folder" $fpath)
 	fi
