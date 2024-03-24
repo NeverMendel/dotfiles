67a98ad610cb219f0373f35ea5358198785fa81c
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2024-03-24 21:48:13 +0000

    Add fzf to zsh

--- zsh/config.sh
+++ zsh/config.sh
@@ -9,7 +9,7 @@ HISTFILE=~/.zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
 
-setopt NO_BG_NICE # don't nice background tasks
+setopt NO_BG_NICE # don't be nice background tasks
 setopt NO_HUP
 setopt NO_LIST_BEEP
 setopt LOCAL_OPTIONS # allow functions to have local options
@@ -23,6 +23,7 @@ setopt COMPLETE_IN_WORD
 setopt APPEND_HISTORY                   # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS             # don't record dupes in history
+setopt HIST_IGNORE_SPACE                # don't record entries starting with a space
 setopt HIST_REDUCE_BLANKS
 
 bindkey '^[^[[D' backward-word
--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -2,6 +2,9 @@ source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zs
 source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $(brew --prefix)/share/zsh-history-substring-search/zsh-history-substring-search.zsh
 
+source $(brew --prefix fzf)/shell/key-bindings.zsh
+source $(brew --prefix fzf)/shell/completion.zsh
+
 # zsh-history-substring-search
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
